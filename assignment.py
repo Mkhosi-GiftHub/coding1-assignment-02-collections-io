9a17fe7ae8badc65c9b1be4e617b6054604d0d64
Mkhosi-GiftHub <Mdluli_Mkhosi@student.ceu.edu>
2026-09-23 17:37:11 +0200

    Added code

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" #this is a definition of variable drink
+sales = {"tea": 3, "coffee": 5} # this is a dictionary
+sales[drink] # this is a lookup in the dictionary
+sold = sold + 1 #This defines a variable sold and adds 1 to it
+print(drink) #This prints the value of the variable drink
+print(sold) #This prints the value of the variable sold
